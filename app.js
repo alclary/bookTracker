e97c0b1cfe327a89f6409e2911b7cd402381bf9d
LoganC <alclary@posteo.net>
2022-10-07 17:51:39 -0500

    prettier formatting

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
+"use strict";
+
 // Define Library Class
 //      collection of books and calculates user-scope metrics
 
-class Library{
-
-    constructor(){
-    
-    }
+class Library {
+    constructor() {}
 }
-
 
 // Define Book Class
 
-class Book{
-
+class Book {
     constructor(author, title, pages, genre, state) {
         this.author = author;
         this.title = title;
         this.pages = pages;
         this.genre = genre;
-        this.state = state;  ///!!! I JUST WROTE STUFF FEEL FREE TO EDIT AS NEEDED LOL
+        this.state = state; ///!!! I JUST WROTE STUFF FEEL FREE TO EDIT AS NEEDED LOL
     }
 }
 // Event listeners for webpage stuff
