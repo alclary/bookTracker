76df3c391827a7635bf16905787ac2f0749f4c8f
LoganC <alclary@posteo.net>
2022-10-07 09:36:17 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Define Library Class
+//      tracks and calculates 
+
+// Define Book Class
+
+// Event listeners for webpage stuff
+//      i.e. defines logic that allows us to create/read/update/delete
+//      books from webpage
+
+// A way to display projected finish time of a book
+
+// Draw a calendar
