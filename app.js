fc704b5b57b92be9ec0b1823cab9cee04630eac6
LoganC <alclary@posteo.net>
2022-10-07 17:44:43 -0500

    Class definition scaffolding
    Co-authored-by: gundrank <gundrank@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 // Define Library Class
 //      collection of books and calculates user-scope metrics
 
+class Library{
+
+    constructor(){
+    
+    }
+}
+
+
 // Define Book Class
 
+class Book{
+
+    constructor(author, title, pages, genre, state) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.genre = genre;
+        this.state = state;  ///!!! I JUST WROTE STUFF FEEL FREE TO EDIT AS NEEDED LOL
+    }
+}
 // Event listeners for webpage stuff
 //      i.e. defines logic that allows us to create/read/update/delete
 //      books from webpage
