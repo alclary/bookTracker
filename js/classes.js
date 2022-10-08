36c90538f3155ffb96225da764429709c3fc870e
LoganC <alclary@posteo.net>
2022-10-08 12:50:00 -0500

    prettier

--- js/classes.js
+++ js/classes.js
@@ -2,26 +2,26 @@
 
 // CLASS DEFINITIONS
 export class Library {
-    constructor() {
-        this.collection = {};
-        this.collectionSpeed = undefined;
-        this.inProgressSpeed = undefined;
-    }
+  constructor() {
+    this.collection = {};
+    this.collectionSpeed = undefined;
+    this.inProgressSpeed = undefined;
+  }
 
-    addBook() {} // Adds book to collection
-    removeBook() {} // Removes book from collection
-    calcSpeedCollection() {} // Calculates a reader's speed based on finished books
-    calcSpeedInProgress() {} // Calculates a reader's speed only based on in-progress books
+  addBook() {} // Adds book to collection
+  removeBook() {} // Removes book from collection
+  calcSpeedCollection() {} // Calculates a reader's speed based on finished books
+  calcSpeedInProgress() {} // Calculates a reader's speed only based on in-progress books
 
-    #save() {} // Saves the library obj to storage, as it exists when save is called 
-};
+  #save() {} // Saves the library obj to storage, as it exists when save is called
+}
 
 export class Book {
-    constructor(author, title, pages, genre, state) {
-        this.author = author;
-        this.title = title;
-        this.pages = pages;
-        this.genre = genre;
-        this.state = state; ///!!! I JUST WROTE STUFF FEEL FREE TO EDIT AS NEEDED LOL
-    }
-};
+  constructor(author, title, pages, genre, state) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.genre = genre;
+    this.state = state; ///!!! I JUST WROTE STUFF FEEL FREE TO EDIT AS NEEDED LOL
+  }
+}
--- js/events.js
+++ js/events.js
@@ -1,66 +1,64 @@
 import { Library, Book } from "./classes.js";
 
-"use strict";
+("use strict");
 
 // VARIABLE DECLARATIONS FOR EVENT LISTENERS
 // newBookForm Input Variables
-const newBookState = document.querySelector('#bookState');
-const newBookTitle = document.querySelector('#bookTitle');
-const newBookAuthor = document.querySelector('#bookAuthor');
-const newBookGenre = document.querySelector('#bookGenre');
+const newBookState = document.querySelector("#bookState");
+const newBookTitle = document.querySelector("#bookTitle");
+const newBookAuthor = document.querySelector("#bookAuthor");
+const newBookGenre = document.querySelector("#bookGenre");
 
 // startedForm Variables
-const startedForm = document.querySelector('#startedForm');
-const newBookPages = document.querySelector('#bookPages');
-const newBookStart = document.querySelector('#bookStarted');
+const startedForm = document.querySelector("#startedForm");
+const newBookPages = document.querySelector("#bookPages");
+const newBookStart = document.querySelector("#bookStarted");
 
 // finishedForm Variables
-const finishedForm = document.querySelector('#finishedForm');
-const newBookFinish = document.querySelector('#bookFinished');
+const finishedForm = document.querySelector("#finishedForm");
+const newBookFinish = document.querySelector("#bookFinished");
 
 // progressForm Variables
-const progressForm = document.querySelector('#progressForm');
-const progressDate = document.querySelector('#progressDate');
-const progressPages = document.querySelector('#progressPages');
+const progressForm = document.querySelector("#progressForm");
+const progressDate = document.querySelector("#progressDate");
+const progressPages = document.querySelector("#progressPages");
 
 // HELPER FUNCTIONS FOR EVENT LISTENERS
 // display base, started, and in-progress parts of form
-function displayInProgressForm () {
-    newBookPages.style.display = '';
-    startedForm.style.display = '';
-    progressForm.style.display = '';
-    finishedForm.style.display = 'none';
+function displayInProgressForm() {
+  newBookPages.style.display = "";
+  startedForm.style.display = "";
+  progressForm.style.display = "";
+  finishedForm.style.display = "none";
 }
 
 // display base, started, and finished parts of form
-function displayFinishedForm () {
-    newBookPages.style.display = '';
-    startedForm.style.display = '';
-    progressForm.style.display = 'none';
-    finishedForm.style.display = '';
+function displayFinishedForm() {
+  newBookPages.style.display = "";
+  startedForm.style.display = "";
+  progressForm.style.display = "none";
+  finishedForm.style.display = "";
 }
 
 // display only base form
-function displayPlannedForm () {
-    newBookPages.style.display = '';
-    startedForm.style.display = 'none';
-    progressForm.style.display = 'none';
-    finishedForm.style.display = 'none';
+function displayPlannedForm() {
+  newBookPages.style.display = "";
+  startedForm.style.display = "none";
+  progressForm.style.display = "none";
+  finishedForm.style.display = "none";
 }
 
 // ENCOMPASSING FUNCTION FOR EVENT LISTENERS
-export default function eventListeners () {
-    // Conditional form display when newBookState changes
-    newBookState.addEventListener('change', () => {
-        if (newBookState.value === "inProgress") {
-            displayInProgressForm();
-        }
-        else if (newBookState.value === "finished") {
-            displayFinishedForm();
-        }
-        else if (newBookState.value === "planned") {
-            displayPlannedForm();
-        }
-    });
-    // Create a new book on 'submit'
-};
+export default function eventListeners() {
+  // Conditional form display when newBookState changes
+  newBookState.addEventListener("change", () => {
+    if (newBookState.value === "inProgress") {
+      displayInProgressForm();
+    } else if (newBookState.value === "finished") {
+      displayFinishedForm();
+    } else if (newBookState.value === "planned") {
+      displayPlannedForm();
+    }
+  });
+  // Create a new book on 'submit'
+}
